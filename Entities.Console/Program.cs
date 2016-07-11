6dd2b76588dfc83ee9dce3572f5f0c77ecc0cf63
Stino <Linksonder@gmail.com>
2016-07-11 12:47:18 +0200

    Initial

---
+++ Entities.Console/Program.cs
@@ -0,0 +1,31 @@
+﻿using Entities.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Summary van alle workshops
+            using(var context = new Entities.Domain.MyEntities())
+            {
+                //Niet vergeten de 'teacher' ook op te vragen!
+                List<Workshop> workshops = context.Workshops.Include("Teacher").ToList();
+
+                System.Console.WriteLine("Workshops");
+                foreach(var workshop in workshops)
+                {
+                    System.Console.WriteLine(String.Format("# {0} - by {1}", workshop.Name, workshop.Teacher.Name));
+                }
+                System.Console.WriteLine(" ");
+
+                System.Console.ReadLine();
+            }
+        }
+    }
+}
