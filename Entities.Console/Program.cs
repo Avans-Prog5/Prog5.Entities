61458eac903c32b2ad8adace5ec36bdf91dc1e4d
Stino <Linksonder@gmail.com>
2016-10-03 10:00:37 +0200

    program nog cleanen

--- Entities.Console/Program.cs
+++ Entities.Console/Program.cs
@@ -11,21 +11,10 @@ namespace Etities.ConsoleApp
     {
         static void Main(string[] args)
         {
-            //Summary van alle workshops
-            using(var context = new Entities.Domain.MyEntities())
-            {
-                //Niet vergeten de 'teacher' ook op te vragen!
-                List<Workshop> workshops = context.Workshops.Include("Teacher").ToList();
-
-                System.Console.WriteLine("Workshops");
-                foreach(var workshop in workshops)
-                {
-                    System.Console.WriteLine(String.Format("# {0} - by {1}", workshop.Name, workshop.Teacher.Name));
-                }
-                System.Console.WriteLine(" ");
-
-                System.Console.ReadLine();
-            }
+            //using(var context = new Entities.Domain.MyEntities())
+            //{
+                
+            //}
         }
     }
 }
